0c64fb6ecd9fcbb6eb0854d9ca453ba461783a6e
thefishgluglu <danielfco.garciacuesta@gmail.com>
2025-07-08 12:45:26 +0200

    cambios Dani

--- webturismo/src/App.jsx
+++ webturismo/src/App.jsx
@@ -1,30 +1,28 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Map from './pages/Map';
-import RoutesPage from './pages/Routes';
-import Events from './pages/Events';
+import Header from './Header'
+import rutaTarracoImg from './assets/ruta-tarraco.png'
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <nav style={{ padding: '1rem', background: '#eee' }}>
-        <Link to="/">Inicio</Link> |{' '}
-        <Link to="/about">Sobre Nosotros</Link> |{' '}
-        <Link to="/map">Mapa</Link> |{' '}
-        <Link to="/routes">Rutas</Link> |{' '}
-        <Link to="/events">Eventos</Link>
-      </nav>
+    <>
+      <Header />
+      
+      <div className="mapa-info-container">
+        {/* Columna izquierda: Detalles */}
+        <div className="info">
+          <h1>Nombre</h1>
+          <p><strong>Descripción:</strong> Aquí puedes añadir texto descriptivo.</p>
+          <p><strong>Servicios:</strong> Restaurantes, zonas wifi, etc.</p>
+        </div>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/routes" element={<RoutesPage />} />
-        <Route path="/events" element={<Events />} />
-      </Routes>
-    </div>
-  );
+        {/* Columna derecha: Mapa */}
+        <div className="mapa">
+          <h2>RUTA HISTÓRICA ANTIGUO TARRACO</h2>
+          <img src={rutaTarracoImg} alt="Ruta Tarraco" />
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
