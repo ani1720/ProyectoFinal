49f91b407e400f0b818d0c2d0a2ce316874325b5
thefishgluglu <danielfco.garciacuesta@gmail.com>
2025-07-11 12:11:20 +0200

    cambios dani

--- webturismo/src/main.jsx
+++ webturismo/src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-const rootElement = document.getElementById('root')
-const root = ReactDOM.createRoot(rootElement)
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
