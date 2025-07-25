34bc24c0b5883225327b847a0452dff65a67c571
AniBnP <NIGHJHUREY17@gmail.com>
2025-07-25 19:21:44 +0200

    intento de arreglo para las rutas

--- webturismo/src/Rutas/RutaList.jsx
+++ webturismo/src/Rutas/RutaList.jsx
@@ -1,15 +1,56 @@
 import { Link } from "react-router-dom";
-import rutas from "/public/data/rutas.json";
+// import rutas from "/public/data/rutas.json";
+import { useEffect, useState } from "react";
+import {collection, doc, getDocs } from "firebase/firestore";
+import { db } from "../firebase/firebaseConfig";
 import "./RutaList.css";
 
+
 const RutaList = () => {
+ const [rutas, setRutas] = useState([]);
+
+  useEffect(() => {
+    const fetchRutas = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Rutas"));
+        const rutasData = querySnapshot.docs.map(doc => ({
+          id: doc.id, // "ruta1", "ruta2", etc.
+          ...doc.data()
+        }));
+        setRutas(rutasData);
+      } catch (error) {
+        console.error("Error al obtener rutas:", error);
+      }
+    };
+
+    fetchRutas();
+  }, []);
+
+  useEffect(() => {
+    const cargarRutas = async () => {
+      const snapshot = await getDocs(collection(db, "Rutas"));
+      const todas = snapshot.doc.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      const filtradas = tipoSeleccionado 
+      ? todas.filter((r) => r.tipo === tipoSeleccionado)
+      : todas;
+      setRutas(filtradas);
+    };
+    cargarRutas();
+  }, [tipoSeleccionado]);
+
   return (
     <div className="pantalla-completa">
       <div className="vertical-layout">
         <h1>Explora Rutas en Tarragona</h1>
 
         <section className="rutas-diagonales">
-          {rutas.slice(0, 3).map((ruta) => (
+          {rutas
+          .filter((ruta) => tipoSeleccionado === "" || ruta.tipo === tipoSeleccionado)
+          .slice(0, 3)
+          .map((ruta) => (
             <Link
               to={`/rutas/${ruta.id}`}
               key={ruta.id}
